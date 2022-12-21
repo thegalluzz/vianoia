5289743702e6a75d9a18779144821625cedb2342
thegalluzz <gabrielet.galluzzo@gmail.com>
2022-12-21 23:43:29 +0100

    implemented getServersideProps to fetch the bored api

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <>
       <Head>
@@ -15,8 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+
+        <h1>Activity: {data.activity}</h1>
+        <p>Type: {data.type}</p>
+        <p>Participants: {data.participants}</p>
         
+
       </main>
     </>
   )
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://www.boredapi.com/api/activity/`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
